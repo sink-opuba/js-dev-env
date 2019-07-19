32f3bf4d7cef90d598140008c4dcd41be7742967
sink-opuba <sinkopus2000@gmail.com>
2019-07-19 11:06:36 +0100

    bundle and lint with webpack and ESlint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,22 @@
 import express from "express";
 import path from "path";
 import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
 
+//configure webpack with express
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  })
+);
+
+//set up express
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
